4186433ea77deef0bfd0eb69c9ab249b6a266a34
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-18 13:28:21 -0300

    JSON

--- syntaxe/Dados_Objetos/JSON/JSON.js
+++ syntaxe/Dados_Objetos/JSON/JSON.js
@@ -21,14 +21,20 @@ console.log(conver)
 //Como acessar json para mostrar no html
 // O fetch é usado para realizar requisições HTTP, pegando dados de servidores ou de arquivos externos(JSON)
 
+const arquivo = fs.readFileSync('eletronicos.json','utf-8');
 
-const arquivo = fs.readFileSync('eletronicos.json', 'utf-8');
+const dado = JSON.parse(arquivo);
 
-const data = JSON.parse(arquivo);
+dado.categoria.produtos.forEach(produto =>{
+    console.log(`nome: ${produto.nome}`)
+})
 
-data.categoria.produtos.forEach(produto =>{
-    console.log(`nome: ${produto.nome} preco: ${produto.preco}`);
-});
 
+const produtosCaros = dado.categoria.produtos.filter(produto => produto.preco > 2000);
+
+console.log(produtosCaros)
+
+
+const formatado = JSON.parse(produtosCaros)
 
 
