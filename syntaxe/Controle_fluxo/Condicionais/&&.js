8619a1da804c23a09910f451545081dbb1543592
LeonardoKaprio <leonardoken02@gmail.com>
2025-10-07 16:02:12 -0300

    Create &&.js

---
+++ syntaxe/Controle_fluxo/Condicionais/&&.js
@@ -0,0 +1,12 @@
+let idade = 25;
+let possuircaiteira = true
+
+
+if(idade >= 18 && possuircaiteira == true)
+{
+    console.log("Sim você pode dirigir")
+}
+else
+{
+    console.log("Não pode dirigir!")
+}
