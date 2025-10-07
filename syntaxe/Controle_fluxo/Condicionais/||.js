2858ac32dd44fe378e758a136c17a053003c2c13
LeonardoKaprio <leonardoken02@gmail.com>
2025-10-07 16:04:24 -0300

    Create ||.js

---
+++ syntaxe/Controle_fluxo/Condicionais/||.js
@@ -0,0 +1,12 @@
+// O operador || serve para dizer basicamente ou, ele so vai retornar true se as 2 condições forem verdadeiras
+
+let maca = true
+
+let banana = false
+
+
+if(maca == true || banana == false){
+    console.log("Você possui alguma fruta")
+}else{
+    console.log("Você não possui nenhuma fruta")
+}
